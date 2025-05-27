3b027ace3cc2032d4a1e8c5ae9d2626b9eb0a8cf
Promode <pramoddutta_96@yahoo.com>
2025-05-27 08:13:08 +0530

    Template

--- src/main/java/com/thetestingacademy/pages/pageObjectModel/normal_POM/LoginPage.java
+++ src/main/java/com/thetestingacademy/pages/pageObjectModel/normal_POM/LoginPage.java
@@ -5,6 +5,8 @@ import com.thetestingacademy.utils.WaitHelpers;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import static com.thetestingacademy.driver.DriverManager.getDriver;
+
 // Page Class
 public class LoginPage {
 
@@ -30,17 +32,17 @@ public class LoginPage {
 
     // Step 2 - Page Actions
     public String loginToVWOLoginInvalidCreds(String user, String pwd) {
-        driver.get(PropertiesReader.readKey("url"));
-        driver.findElement(username).sendKeys(user);
-        driver.findElement(password).sendKeys(pwd);
-        driver.findElement(signButton).click();
+        getDriver().get(PropertiesReader.readKey("url"));
+        getDriver().findElement(username).sendKeys(user);
+        getDriver().findElement(password).sendKeys(pwd);
+        getDriver().findElement(signButton).click();
 
         // Wait - Thread sleep
 //        WaitHelpers.waitJVM(5000);
 
-        WaitHelpers.checkVisibility(driver,error_message,3);
+        WaitHelpers.checkVisibility(getDriver(),error_message,3);
 
-        String error_message_text = driver.findElement(error_message).getText();
+        String error_message_text = getDriver().findElement(error_message).getText();
         return error_message_text;
     }
 }
